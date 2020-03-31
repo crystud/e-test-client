e580c7eb6da759cbb72dd715894ddb0a7471bdde
Oleh Kirashchuk <olegkirashchuk@gmail.com>
2020-03-31 20:24:45 +0300

    clear verify requests page

--- src/views/VerifyRequests.vue
+++ src/views/VerifyRequests.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="verify-requests">
-    <app-search-bar class="search"></app-search-bar>
+    <div class="search">
+      <div class="additional-switch">
+        <div class="label">Верифіковані</div>
+
+        <app-switch
+          :isOn="areVerificated"
+          class="switch"
+          @switch="areVerificated = !areVerificated"
+        ></app-switch>
+      </div>
+
+      <app-search-bar class="bar"></app-search-bar>
+    </div>
 
     <div class="list">
       <app-verify-candidate
@@ -14,11 +26,18 @@
 <script>
 import AppSearchBar from '../components/ui/AppSearchBar.vue'
 import AppVerifyCandidate from '../components/templates/superadmin/AppVerifyCandidate.vue'
+import AppSwitch from '../components/ui/AppSwitch.vue'
 
 export default {
   components: {
     AppSearchBar,
     AppVerifyCandidate,
+    AppSwitch,
+  },
+  data() {
+    return {
+      areVerificated: false,
+    }
   },
 }
 </script>
@@ -26,8 +45,35 @@ export default {
 <style lang="less" scoped>
 .verify-requests {
   .search {
-    background: var(--color-bg-dark);
     border-radius: 10px;
+
+    display: flex;
+    align-items: center;
+
+    &, .bar {
+      background: var(--color-bg-dark);
+    }
+
+    .additional-switch {
+      padding: 20px;
+      border-right: 1px solid var(--color-bg-main);
+
+      .switch {
+        margin: 10px auto 0;
+      }
+    }
+
+    @media screen and (max-width: 400px) {
+      flex-direction: column;
+
+      .additional-switch {
+        border-right: 0;
+        border-bottom: 1px solid var(--color-bg-main);
+
+        text-align: center;
+        width: 100%;
+      }
+    }
   }
 
   .list {
